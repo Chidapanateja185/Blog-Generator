0c2f0d7ef54cc6a57d95786343bae7c610b47869
Teja Chidapana <110326598+Chidapanateja185@users.noreply.github.com>
2026-04-30 21:13:32 +0530

    Initial commit

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=[
+        "http://localhost:3000",
+        "https://blog-generator-app-nu.vercel.app"
+    ],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
