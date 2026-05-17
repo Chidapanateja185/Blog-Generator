7a07db426913c426b0294164659b46ce183d4af4
Teja Chidapana <110326598+Chidapanateja185@users.noreply.github.com>
2026-05-17 19:29:01 +0530

    updated config

--- src/config/firebase_config.py
+++ src/config/firebase_config.py
@@ -12,11 +12,13 @@ if not firebase_admin._apps:
     firebase_json = os.getenv("FIREBASE_JSON")
 
     if firebase_json:
-        # FIREBASE_JSON contains full JSON string
+
         cred_dict = json.loads(firebase_json)
+
         cred = credentials.Certificate(cred_dict)
+
     else:
-        # Local JSON file path
+
         cred = credentials.Certificate(
             "src/config/firebase-adminsdk.json"
         )
